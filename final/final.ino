c74e43cf2d3b8a743e03db0d95fa168eda87fdee
Dhanush gowda u <92075046+Dhanush678@users.noreply.github.com>
2023-07-03 08:40:03 +0530

    Update final.ino

--- final/final.ino
+++ final/final.ino
@@ -1,21 +1,25 @@
-
-int ena = 5;
-int m1a = 6;
-int m1b = 7;
-int m2a = 8;
-int m2b = 9;
-int enb = 10;
+#include <Servo.h>
+int ena=8;
+int m1a = 9;
+int m1b = 10;
+int m2a = 11;
+int m2b = 12;
+int enb=13;
 char val;
+Servo myservo;
+#define servoPin 3
+int angle = 0;
 
 void setup() 
 {  
 pinMode(m1a, OUTPUT);  // Digital pin 10 set as output Pin
-pinMode(m1b, OUTPUT);  // Digital pin 11 set as output Pin
+pinMode(m1b, OUTPUT);
+pinMode (ena,OUTPUT);
+pinMode(enb,OUTPUT);// Digital pin 11 set as output Pin
 pinMode(m2a, OUTPUT);  // Digital pin 12 set as output Pin
-pinMode(m2b, OUTPUT);
-  pinMode(ena, OUTPUT);
-    pinMode(enb, OUTPUT);// Digital pin 13 set as output Pin
+pinMode(m2b, OUTPUT);  // Digital pin 13 set as output Pin
 Serial.begin(9600);
+ myservo.attach(servoPin);
 }
 
 void loop()
@@ -31,9 +35,9 @@ void loop()
       digitalWrite(m1a, HIGH);
       digitalWrite(m1b, LOW);
       digitalWrite(m2a, HIGH);
-      digitalWrite(m2b, LOW); 
-      analogWrite(ena, 255); 
-      analogWrite(enb, 255);
+      digitalWrite(m2b, LOW);  
+      digitalWrite(ena,255);
+      digitalWrite(enb,255);
     }
   else if(val == 'B') // Backward
     {
@@ -41,24 +45,25 @@ void loop()
       digitalWrite(m1b, HIGH);
       digitalWrite(m2a, LOW);
       digitalWrite(m2b, HIGH); 
-      analogWrite(ena, 255); 
-      analogWrite(enb, 255);
-      
+       digitalWrite(ena,255);
+      digitalWrite(enb,255);
     }
   
     else if(val == 'L') //Left
     {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
+   
     digitalWrite(m2a, HIGH);
     digitalWrite(m2b, LOW);
+        digitalWrite(ena,255);
+      digitalWrite(enb,255);
     }
     else if(val == 'R') //Right
     {
     digitalWrite(m1a, HIGH);
     digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW); 
+   
+        digitalWrite(ena,255);
+      digitalWrite(enb,255);
     }
     
   else if(val == 'S') //Stop
@@ -67,43 +72,14 @@ void loop()
     digitalWrite(m1b, LOW);
     digitalWrite(m2a, LOW);
     digitalWrite(m2b, LOW); 
-    analogWrite(ena, 255); 
-      analogWrite(enb, 255);
+        digitalWrite(ena,255);
+      digitalWrite(enb,255);
     }
-  else if(val == 'I') //Forward Right
-    {
-    digitalWrite(m1a, HIGH);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
-    analogWrite(ena, 255); 
-      analogWrite(enb, 255);
+    else if(val=='G'){
+        myservo.write(90);
     }
-  else if(val == 'J') //Backward Right
-    {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, HIGH);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
-    analogWrite(ena, 255); 
-      analogWrite(enb, 255);
+    else if(val=='O')                                                                                           {
+        myservo.write(180);
     }
-   else if(val == 'G') //Forward Left
-    {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, HIGH); 
-    digitalWrite(m2b, LOW);
-    analogWrite(ena, 255); 
-      analogWrite(enb, 255);
-    }
-  else if(val == 'H') //Backward Left
-    {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, HIGH); 
-    analogWrite(ena, 255); 
-      analogWrite(enb, 255);
-    }
+ 
 }
